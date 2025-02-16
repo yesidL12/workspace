6e6f36eeb3adf4e56993c81ff0d6854e713b96eb
yesidL12 <yesidserio@gmail.com>
2025-02-15 23:36:37 -0500

    Agregar diccionario y función de agenda para gestionar contactos

--- Logica de programacion/03- estructuras de datos.py
+++ Logica de programacion/03- estructuras de datos.py
@@ -47,3 +47,90 @@ mySets.update # Lo que hace es ampliar los datos que tenemnos en el set
 mySets = set(sorted(mySets)) # El set no se puede ordenar
 print(mySets)
 print(type(mySets))
+
+'Diccionario'
+
+
+myDict: dict = { # La forma en que metemos los datos es: clave - valor
+"name":"Brais", 
+"subname":"Moure", 
+"alias": "MoureDev", 
+"edad": "36"
+}
+
+myDict["email"] = "mouredev@gmail.com" #inserción
+print(myDict["name"]) # Acceso
+myDict["edad"] = "37" # Actualización
+myDict = dict(sorted(myDict.items())) # Ordenación
+# del myDict["subname"] # Eliminación
+print(myDict)
+print(type(myDict))
+
+'Extra'
+
+def myAgenda():
+
+    agenda = {} # Diccionario vacio donde se guardará la información que de el usuario
+
+    while True: # Bucle infito
+
+        #opciones del usuario
+        print("")
+        print("1. Buscar contacto")
+        print("2. Insertar contacto")
+        print("3. Actualizar contacto")
+        print("4. Eliminar contacto")
+        print("5. Salir")
+
+        option = input("\nSelecciona una opción: ") # El usuario seleciona que desea hacer
+
+        match option: # Hace las veces de Switch
+            case "1":# Buscar contacto
+                name = input("Introduce el nombre del contacto a buscar: ")
+                if name in agenda:
+                    print(
+                        f"El número del teléfono de {name} es {agenda[name]}.")
+                else:
+                    print(
+                        f" El contacto {name} no existe"
+                    )
+                #pass # Evita errores ya que impide que se ejhecute todo el bucle
+            case "2": #insertar contacto
+                name = input("\n Introduce el nombre del contacto: ")
+                phone = input("\n Introduce el teléfono del contacto: ")
+                #Hacemos la validación de que sea un numnero de telefono valido
+                if phone.isdigit() and len(phone) > 0 and len(phone) <= 11:
+                    agenda[name] = phone
+                else:
+                    print(
+                        "Debes introducir un número de telefono valido")
+            case "3": # Actualización de contacto
+                name = input("Introduce el nombre del contacto a actualizar: ")
+                if name in agenda:
+                    phone = input("\nIntroduce el teléfono del contacto: ")
+                    if phone.isdigit() and len(phone) > 0 and len(phone) <= 11:
+                        agenda[name] = phone
+                    else:
+                        print("Debes introducir un número de teléfono valido")
+                else:
+                    print(
+                        f"El contacto {name} no existe"
+                    )
+            case "4": # Eliminar contacto
+                name = input("Introduce el nombre del contacto a eliminar: ")
+                if name in agenda:
+                    del agenda[name]
+                    print(
+                        f"El contacto {name} ha sido eliminado"
+                    )
+                else:
+                    print(
+                        f"El contacto {name} no existe"
+                    )
+            case "5":
+                print("Saliendo de la agenda.")
+                break # Lo que hace es aslir del bucle que se está ejecutando.
+            case _: # Opción por Default
+                print("Opción no valida. ELige una opción del 1 al 5.")
+
+myAgenda()
